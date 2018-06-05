41c4101aba80c5685cc8862e857974546df8f371
fish34 <fish34@mail.ru>
2018-06-05 11:54:51 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-rom django.views.generic import RedirectView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
