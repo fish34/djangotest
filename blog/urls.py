dcc163d235901ee3a48408106497fec640921541
root <fish34@mail.ru>
2018-06-05 17:25:47 +0300

    Added view and template for detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^test/$', RedirectView.as_view(url=r'https://google.com')), #google
 ]
