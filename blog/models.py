1f35dd17014b6ffb8957e3c4ca2e9d6c9d08bd2f
root <fish34@mail.ru>
2018-06-09 10:55:41 +0300

    updated

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
@@ -29,5 +32,6 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment=True
         self.save()
+
     def __str__(self):
         return self.text
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from django.contrib import messages
-from .models import Post
+from django.contrib import messages,auth
+from .models import Post,Comment
 from .forms import PostForm,CommentForm
 
 
@@ -79,3 +79,15 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return  render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
